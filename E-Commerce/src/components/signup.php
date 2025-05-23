291d0922cf71caabcc3d88087bebc2a540ecc2eb
Justine Avio <justinejeckho.avio@cvsu.edu.ph>
2025-05-24 04:38:02 +0800

    Updating signup form but not finished

--- E-Commerce/src/components/signup.php
+++ E-Commerce/src/components/signup.php
@@ -48,8 +48,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['checkbox'])) {
                     <div class = checkbox-container> 
                         <label>
                             <input type="checkbox" id="checkbox" name="checkbox">
-                            I agree to the&nbsp;<b><a href="#">Terms and Conditions</a></b>&nbsp; and &nbsp; <b><a href="#">Privacy Policies</a></b>
+                            I agree to the&nbsp;<b><a href="#" id="OpenTerms">Terms and Conditions</a></b>&nbsp; and &nbsp; <b><a href="#" id="Privacy">Privacy Policies</a></b>
                         </label>
+                    </div>
+                    <div class="modal" id="modalTerms">
+                        <div class="modal-inner">
+                            <h2> Terms and Conditions</h2>
+                            <h6>Effective Date: 04/10/2025</h6>
+                            <h6>Company Name: SoundStage Inc.</h6>
+                            <ol type="1" class= "list-wrapper">
+                                <li>Introduction</li>
+                                <li>Eligibility</li>
+                                <li>Purchases</li>
+                                <li>Pricing and Payment</li>
+                                <li>Shipping and Delivery</li>
+                                <li>Returns and Refunds</li>
+                                <li>Intellectual Property</li>
+                                <li>Probihited Activities</li>
+                                <li>Limitation of Liability</li>
+                                <li>Changes of these Terms</li>
+                                <li>Governing Law</li>
+                                <li>Contact Information</li>
+                            </ol>
+                            <button type="button" class="close-modal" id="closeTerms">Close</button>
+                        </div>
+                    </div>
+                    <div class="modal" id="modalPrivacy">
+                        <div class="modal-inner">
+                            <h2> Privacy Policy</h2>
+                            <h6>Effective Date: 04/10/2025</h6>
+                            <h6>Company Name: SoundStage Inc.</h6>
+                            <ol type="1" class= "list-wrapper">
+                                <li>Introduction</li>
+                                <li>Information We Collect</li>
+                                <li>How we Use Your Information</li>
+                                <li>Sharing your information</li>
+                                <li>Cookies</li>
+                                <li>Security</li>
+                                <li>Your Rights</li>
+                                <li>Third-Party Links</li>
+                                <li>Changes to THis Privacy Policy</li>
+                                <li>Contact Us</li>
+                            </ol>
+                            <button type="button" class="close-modal" id="closePrivacy">Close</button>
+                        </div>
                     </div>
                     <button type="submit" name ="send"><b>Register</b></button>
                     <p id ="Notice" >Already have an account? <b><a href="../components/login.php">Login</a></b></p>
@@ -66,5 +108,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['checkbox'])) {
     integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" 
     crossorigin="anonymous"></script>
     <script src="../script/test.js"></script>
+    <script>
+        const openterms = document.getElementById('OpenTerms');
+        const modalTerms = document.getElementById('modalTerms');
+        const closeterms  = document.getElementById('closeTerms');
+
+        openterms.addEventListener('click', function(event) {
+            event.preventDefault();
+            modalTerms.classList.add('open');
+        });
+        closeterms.addEventListener('click', function(event) {
+            modalTerms.classList.remove('open');
+        });
+
+        const openPrivacy = document.getElementById('Privacy');
+        const modalPrivacy = document.getElementById('modalPrivacy');
+        const closePrivacy = document.getElementById('closePrivacy');
+        
+        openPrivacy.addEventListener('click', function(event){
+            event.preventDefault();
+            modalPrivacy.classList.add('open');
+        });
+        closePrivacy.addEventListener('click', function(event){
+            modalPrivacy.classList.remove('open');
+        });
+
+    </script>
 </body>
 </html>
